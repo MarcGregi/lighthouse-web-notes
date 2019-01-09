6a6284b83577c1290c03cda10c883d22c4d5a567
marcgregi <marc@hypedigital.co>
2019-01-09 16:12:49 +0000

    Lecture notes from day 2

---
+++ arrayslecture.js
@@ -0,0 +1,42 @@
+// approach to tackling the problem
+
+// talk about the big problem
+
+// read specs / requirements
+
+// try to come up with a solution
+
+function addArray(myArray, start) {
+  if (!Array.isArray(myArray)) {
+    return false;
+  }
+  if (!start) {
+    start = 0;
+  } return start
+
+
+}
+
+
+// possible solution: a function that takes arrays
+
+// make sure it is an array
+
+// maybe it is a long string
+
+// str.split(', '); - splits the strings in the array
+
+// calculates the sum of all changes
+
+// code the solution
+
+// node debug arrays.js
+
+// Number(myArray[i]) - converts string to number
+
+// var data = `
+
+// data.split(`/n)
+
+
+
