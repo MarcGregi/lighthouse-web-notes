f0ce35d384dd3764d5a8e1efd2f82f2d1771d4f6
marcgregi <marc@hypedigital.co>
2019-01-11 16:04:34 +0000

    Friday lecture learning about higher order functions and requiring modules

---
+++ refactoringlectures.js
@@ -0,0 +1,13 @@
+function greatestGeneration () {
+  for (person of ancestory) {
+    if (person.age >= 1900 && person.age <= 1930) {
+      if (person.sex === "m");
+    }
+  }
+  return person;
+}
+
+function isGreatestGeneration (person) {
+  return person.born >= 1900 && person.born < 1930;
+}
+
