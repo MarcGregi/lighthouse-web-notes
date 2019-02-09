f979f762986aa42294fcb5f546c9396e36ca5cf3
marcgregi <marc@hypedigital.co>
2019-02-09 00:33:00 +0000

    Visualising how async can work and why it would be useful.

---
+++ asyncPractice.js
@@ -0,0 +1,35 @@
+// Stack overflow
+
+const foo = () => {
+    foo(); 
+}
+foo(); 
+
+
+
+// aysnc programming practice
+
+  console.log('1'); 
+  setTimeout(() => {
+       console.log('2'); 
+  }, 2000) 
+  console.log('3'); 
+
+
+// CALL STACK 
+
+setTimeout(); 
+// WEB API 
+
+
+// CALLBACK QUEUE 
+
+
+// EVENT LOOP
+
+element.addEventListener('click', () => {
+    console.log(`Baited ${click}`); 
+}); 
+
+
+
